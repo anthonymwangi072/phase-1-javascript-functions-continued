05ecf40cc64a131e4d9197dba508a9fff9450146
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-17 23:51:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(interest = "roller-skate") {
+    return (`This Saturday, I want to ${interest}!`)
+}
+function mondayWork(interest="go to the office") {
+    return (`This Monday, I will ${interest}.`);
+
+}
+const wrapAdjective = function(wrap="*"){
+    return function(parameter="special"){
+        return `You are ${wrap}${parameter}${wrap}!`
+    }
+
+}
